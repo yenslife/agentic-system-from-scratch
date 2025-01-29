1ad5e6440b1097d0e0cbd5d79de08d62919f6a72
yenslife <77geo5rge6@gmail.com>
2025-01-30 02:04:53 +0800

    refactor: CompletionResponse type
    因為我覺得要一直用 choices[0] 很煩 == 所以抽象了這一層

--- function_calling_llm.py
+++ function_calling_llm.py
@@ -11,10 +11,10 @@ from tools.fruit import FRUIT_FUNCTION_SCHEMA as schema
 # 定義兩個工具
 tools = [schema["get_remain_fruit"], schema["get_fruit_info"]]
 
-response = completion(hello_message, tools)["message"]
-response_with_tool = completion(tool_message, tools)["message"]
-response_with_multiple_tools = completion(multiple_tool_message, tools)["message"]
+response = completion(hello_message, tools)
+response_with_tool = completion(tool_message, tools)
+response_with_multiple_tools = completion(multiple_tool_message, tools)
 
-print("單純聊天的回覆",response)
-print("偵測到需要工具的回覆", response_with_tool)
-print("偵測到需要多個工具的回覆", response_with_multiple_tools)
+print("單純聊天的回覆",response.message)
+print("偵測到需要工具的回覆", response_with_tool.message)
+print("偵測到需要多個工具的回覆", response_with_multiple_tools.message)
--- model_providers/openai/llm.py
+++ model_providers/openai/llm.py
@@ -1,18 +1,25 @@
+from dataclasses import dataclass
 from openai import OpenAI
-from openai.types.chat import ChatCompletion
+from openai.types.chat import ChatCompletion, ChatCompletionMessage
 from dotenv import load_dotenv
 
 ## set ENV variables
 load_dotenv()
 client = OpenAI()
 
-def completion(messages, tools=None, model="gpt-4o-mini"):
+@dataclass
+class CompletionResponse():
+    response: ChatCompletion
+    message: ChatCompletionMessage
+
+def completion(messages, tools=None, model="gpt-4o-mini") -> CompletionResponse:
+    """因為我覺得要一直用 choices[0] 很煩所以抽象了這一層"""
     response: ChatCompletion = client.chat.completions.create(
         model=model,
         messages=messages,
         tools=tools,
     )
-    return {
-        "response": response,
-        "message": response.choices[0].message
-    }
+    return CompletionResponse(
+        response=response,
+        message=response.choices[0].message
+    )
